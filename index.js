178a9d017d0e24c3821e73f2d415ece0b21fbd40
rubayetseason <season.ss2001@gmail.com>
2022-09-03 14:23:30 +0600

    cards done

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const newsbar = async () => {
 }
 
 const displayNewsbar = (news) => {
-    console.log(news);
     const newsbarContainer = document.getElementById('newsbar-container');
     news.forEach(singleNews => {
         const newsDiv = document.createElement('div');
@@ -19,17 +18,61 @@ const displayNewsbar = (news) => {
 
 const newsfeed = async (id) => {
     // console.log(id); 01 02 03 04
-    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     console.log(url);
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-
+    displayNews(data);
 }
+
+const displayNews = (news) => {
+    const newsContainer = document.getElementById('newsfeed-container');
+    newsContainer.innerHTML = "";
+// items found here 
+const newsNumber = document.getElementById('items-found');
+newsNumber.innerText = `${news.data.length} items found for this category`;
+// items found here
+
+    news.data.forEach(singleNews => {
+        console.log(singleNews);
+        const newsDiv = document.createElement('div');
+        newsDiv.innerHTML = `
+        <div class="card mb-3" style="width: 100%;">
+                    <div class="row g-0">
+                        <div class="col-md-3">
+                            <img src="${singleNews.thumbnail_url}" class="img-fluid rounded-0" style="width: 100%;">
+                        </div>
+                        <div class="col-md-9">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">${singleNews.title}</h5>
+                                <p class="card-text">${singleNews.details.slice(0, 300)}...</p>
+                                <div id="main-div" class="d-flex align-items-center">
+
+                                <div class="d-flex flex-column" style="width: 30%;">
+                                <img src="${singleNews.author.img}" style="width: 15%;" class="rounded-5">
+                                <p class="fw-semibold">${singleNews.author.name ? singleNews.author.name : "No author found"}</p>
+                                </div>
+
+                                <div style="width: 30%;">
+                                <p>Views: ${singleNews.total_view}</p>
+                                </div>
+                                <div style="width: 30%;">
+                                <button class="btn btn-danger">More</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    });
+}
+
 
 newsbar();
 
 
-document.getElementById('blog-id').addEventListener('click', function(){
+document.getElementById('blog-id').addEventListener('click', function () {
     window.location.href = 'blog.html';
 })
